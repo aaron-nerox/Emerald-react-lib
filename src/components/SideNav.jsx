776e8c6e905ef74f12b348370dd13a1a2c4ff2cf
The X <haroune.official2@gmail.com>
2022-03-05 16:09:12 +0100

    add: side navigation panel

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,14 +1,14 @@
 
 
-const SideNav = (props) => {
+export const SideNav = (props) => {
 
     let navItems = props.navItems;
     let activeItemIndex = props.activeItemIndex;
 
     return (
-        <div className="w-[22%] h-screen p-2 bg-primary-accent/50
+        <div className="w-[22%] h-screen max-h-screen overflow-y-auto p-2 bg-primary-accent/50
         fixed left-0 top-0 inline-flex flex-col items-start">
-            <div className="w-full h-[150px] bg-primary-screen rounded-lg mx-auto 
+            <div className="w-full min-h-[150px] bg-primary-screen rounded-lg mx-auto 
                 mb-5 p-3 grid content-center justify-items-center">
                     {props.header}
             </div>
@@ -17,15 +17,16 @@ const SideNav = (props) => {
                 navItems.map((item, index) => {
                     return (
                         <li key={index} onClick={() => props.onItemClick(index)} 
-                            className="w-full mx-auto" >
+                            className="w-full mx-auto">
 
                             <div className={`w-full mx-auto inline-flex flex-row 
                                 items-center m-2 py-1 hover:bg-primary-screen rounded-lg transition-all 
                                 ${(index === activeItemIndex)? "bg-primary-screen text-primary stroke-primary" : "stroke-primary-surface-black"}`}>
                                 <div className="h-7 w-7 m-2">
-                                    {item.icon} 
+                                    {item.icon}
                                 </div>
-                                <p className="mr-3">{item.title}</p>
+                                <p className="mr-3 line-clamp-1 text-ellipsis">{item.title}</p>
+                                
                             </div>
                         </li>
                     )
@@ -35,6 +36,3 @@ const SideNav = (props) => {
         </div>
     )
 }
-
-
-export default SideNav;
---
+++ src/stories/SideNav.stories.js
@@ -0,0 +1,43 @@
+import { SideNav } from "../index";
+import Icon from "../assets/icons/result.svg";
+let Icon2 = <svg fill="none" stroke="current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="m22 10v5c0 5-2 7-7 7h-6c-5 0-7-2-7-7v-6c0-5 2-7 7-7h5" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/>
+             <path d="m22 10h-4c-3 0-4-1-4-4v-4l8 8z" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/>
+             <path d="m8.32 12h2.65c0.27 0 0.61 0.18 0.75 0.4l1.13 1.7c0.23 0.34 0.18 0.85-0.11 1.14l-2.46 2.46c-0.35 0.35-0.93 0.35-1.28 0l-2.46-2.46c-0.29-0.29-0.34-0.8-0.11-1.14l1.13-1.7c0.16-0.22 0.5-0.4 0.76-0.4z" clip-rule="evenodd" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5"/>
+        </svg>
+
+export default {
+    title: "Navigation/SideNav",
+    component: SideNav,
+}
+
+const Template = (args) => {
+    return <SideNav {...args} />
+}
+
+let items = [
+    {
+        icon: Icon2,
+        title: "Nav Item"
+    },
+    {
+        icon: <img src={Icon} />,
+        title: "Nav Item"
+    },
+    {
+        icon: <img src={Icon} />,
+        title: "Nav Item"
+    },
+    {
+        icon: <img src={Icon} />,
+        title: "Nav Item dgfg dgfdfgd fgdfgd gdfgdf"
+    },
+    
+]
+
+export const Primary = Template.bind({})
+Primary.args = {
+    activeItemIndex: 0,
+    navItems: items,
+    header: <div></div>
+}
